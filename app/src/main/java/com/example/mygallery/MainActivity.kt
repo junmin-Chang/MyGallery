410da4ade12d5469c8e64dcd6e819ac529b57d3e
junminChang <chjm219@kookmin.ac.kr>
2020-08-03 14:14:58 +0900

    permission_2

--- app/src/main/java/com/example/mygallery/MainActivity.kt
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,25 +1,73 @@
 package com.example.mygallery
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_READ_EXTERNAL_STORAGE = 1000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 권한이 부여되었는지 확인 1
+        if (ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
-        getAllPhotos()
+            // 권한이 허용되지 않음 2
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+
+                // 이전에 이미 권한이 거부되었을 때 설명3
+                alert("사진 정보를 얻으려면 외부 저장소 권한이 필수로 필요합니다.", "권한이 필요한 이유") {
+                    yesButton {
+                        //권한 요청
+
+                        ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_READ_EXTERNAL_STORAGE)
+
+                    }
+
+                    noButton {  }
+                }.show()
+
+            } else {
+
+                // 권한 요청4
+                ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_READ_EXTERNAL_STORAGE)
+            }
+
+
+        } else {
+            // 권한이 이미 허용됨 5
+            getAllPhotos()
+
+        }
+
+
+
     }
 
 
     private fun getAllPhotos() {
-        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null,
-                null,
-                null,
-                MediaStore.Images.ImageColumns.DATE_TAKEN + "DESC")
+        val cursor = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            MediaStore.Images.ImageColumns.DATE_TAKEN + "DESC"
+        )
     }
 }
 
