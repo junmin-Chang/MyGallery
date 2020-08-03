12cb60d0a2d06aaa0bd93c0a804066aa912929b3
junminChang <chjm219@kookmin.ac.kr>
2020-08-03 12:00:31 +0900

    permission access & load photos

---
+++ app/src/main/java/com/example/mygallery/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mygallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        getAllPhotos()
+    }
+
+
+    private fun getAllPhotos() {
+        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null,
+                null,
+                null,
+                MediaStore.Images.ImageColumns.DATE_TAKEN + "DESC")
+    }
+}
+
+
